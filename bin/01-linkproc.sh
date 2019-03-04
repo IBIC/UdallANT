8925409f91479c6b320580197db89f29a31b9330
Trevor <tkmday@uw.edu>
2019-03-04 12:35:48 -0800

    add README

--- bin/01-linkproc.sh
+++ bin/01-linkproc.sh
@@ -8,9 +8,12 @@ for i in RC4* ; do
 
 	for j in 1 2 ; do
 
-		cd session${j} 
+		cd session${j}
 
-		ln -s /mnt/praxic/udallpilot/bin/00-extract.sh .
+		ln -s /mnt/praxic/udallpilot/bin/subject-01-extract.sh 01-extract.sh
+		ln -s /mnt/praxic/udallpilot/bin/subject-02-makeafniscript.sh \
+				02-afniscript.sh
+
 		ln -s /mnt/praxic/udallpilot/lib/Makefile .
 
 		cd ..
